a2761df01ebb71cb9c466c512b70c78f6ddbb8fd
Akhil Gajulavarti <100663405+akhilUTA1004@users.noreply.github.com>
2026-02-20 10:46:22 -0600

    Started adding thoughts

--- src/Main.java
+++ src/Main.java
@@ -13,23 +13,46 @@ void main() {
         IO.println("i = " + i);
     }
 
+    System.out.println("##############################################################################");
+    TryThoughts.printEmailDomain("akhil.gajulavarti@hotmail.com");
+    System.out.println("##############################################################################");
+    TryThoughts.printEmailHostName("akhil.gajulavarti@abc.def.ghi.jkl.com");
+    System.out.println("##############################################################################");
     TryString.findFirstNonRepeatedCharacterA1("swiss");
+    System.out.println("##############################################################################");
     TryString.findFirstNonRepeatedCharacterA2("swiss");
+    System.out.println("##############################################################################");
     TryString.checkAnagrams("listen", "silent");
+    System.out.println("##############################################################################");
     TryString.findFirstRepeatedCharacter("programming");
+    System.out.println("##############################################################################");
     TryString.countCharacterFrequency("success");
+    System.out.println("##############################################################################");
     TryString.reverseEachWord("Java Developer");
+    System.out.println("##############################################################################");
     TryString.findLongestWordInSentence("I am learning Spring Boot");
+    System.out.println("##############################################################################");
     TryString.checkStringRotation("waterbottle", "erbottlewat");
+    System.out.println("##############################################################################");
     TryString.findDuplicateWords("This is a test. This test is only a test.");
+    System.out.println("##############################################################################");
     TryString.hasStringOnlyDigits("123456");
+    System.out.println("##############################################################################");
     TryString.removeDuplicateCharacters("programming");
+    System.out.println("##############################################################################");
     TryString.recursiveStringPermutations("abc"," ");
+    System.out.println("##############################################################################");
     TryString.findMostRepeatedCharacter("characteristics");
+    System.out.println("##############################################################################");
     TryString.checkUniqueCharacters("abcdefg");
+    System.out.println("##############################################################################");
     TryString.reverseWordsOrder("This is a sample sentence");
+    System.out.println("##############################################################################");
     TryString.stringCompression("aaabbbcccaaa");
+    System.out.println("##############################################################################");
     TryString.longestPalindromicSubstring("babad");
+    System.out.println("##############################################################################");
     LeetCodeProblems.waterContainer();
+    System.out.println("##############################################################################");
 
 }
---
+++ src/tryouts/TryThoughts.java
@@ -0,0 +1,40 @@
+package tryouts;
+
+public class TryThoughts {
+    /*
+     *
+     * Print the domain of an email address
+     * Example: For the email "
+     * Output: Domain: hotmail.com
+     *
+     * */
+    public static void printEmailDomain(String email) {
+        System.out.println("Email : " + email);
+        int atIndex = email.indexOf('@');
+        if (atIndex != -1 && atIndex < email.length() - 1) {
+            String domain = email.substring(atIndex + 1);
+            System.out.println("Email Domain : " + domain);
+        } else {
+            System.out.println("Invalid email address.");
+        }
+    }
+
+    public static void printEmailHostName(String email) {
+        int at = email.lastIndexOf('@');
+        if (at < 0 || at == email.length() - 1) {
+            System.out.println("Invalid email address.");
+            return;
+        }
+
+        String host = email.substring(at + 1);          // e.g. abc.def.cgf.hotmail.in
+        int lastDot = host.lastIndexOf('.');
+        if (lastDot <= 0 || lastDot == host.length() - 1) {
+            System.out.println("Invalid email address.");
+            return;
+        }
+        System.out.println("Email : " + email);
+        System.out.println("Email Host name : " + host.substring(0, lastDot)); // e.g. abc.def.cgf.hotmail
+    }
+
+
+}
